ea84baf491788b48fff17a08b51fc566c3a450a3
mupadhya-ta <mupadhya@uottawa.ca>
2023-02-26 20:20:00 -0500

    added chnages

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 from werkzeug.utils import secure_filename
 from keras.models import load_model
-import tensorflow as tf
+import tflite as tf
 import os
 import numpy as np
 
