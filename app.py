2b601fcee78a75c325c931a4081a526c7129f94a
mupadhya-ta <mupadhya@uottawa.ca>
2023-02-28 00:18:34 -0500

    using tflite

--- app.py
+++ app.py
@@ -1,39 +1,53 @@
 from flask import Flask, jsonify, request
 from werkzeug.utils import secure_filename
-from keras.models import load_model
-import tensorflow as tf
-import os
 import numpy as np
-
-
+import tensorflow.lite as tflite
+import os
 
 app = Flask(__name__)
 
-# Load the trained model
-model = load_model("models/pneumonia.h5")
+# Load the TFLite model
+model_path = "models/pneumonia.tflite"
+interpreter = tflite.Interpreter(model_path=model_path)
+interpreter.allocate_tensors()
+
+# Get the input and output tensors
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
 @app.route('/', methods=['GET'])
 def home():
     return "Hello From Flask"
+
 @app.route('/predict', methods=['POST'])
 def predict():
     # Get the uploaded file
     file = request.files['file']
-    
+
     # Save the file to disk
     filename = secure_filename(file.filename)
     file_path = os.path.join('uploads', filename)
     file.save(file_path)
 
     # Load the image and preprocess it
-    img = tf.keras.utils.load_img(file_path, target_size=(128,128))
-    img = tf.keras.utils.img_to_array(img)
-    img = np.expand_dims(img, axis = 0)
+    img = tflite.keras.preprocessing.image.load_img(file_path, target_size=(128, 128))
+    img = tflite.keras.preprocessing.image.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    img = np.float32(img)
 
-    # Make a prediction with the model
-    prediction = model.predict(img)
-    predicted_label = np.argmax(prediction)
-    class_names={0: 'NORMAL', 1: 'PNEUMONIA'}
+    # Set the input tensor
+    interpreter.set_tensor(input_details[0]['index'], img)
+
+    # Run the model
+    interpreter.invoke()
+
+    # Get the output tensor
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    output_data = np.squeeze(output_data)
+
+    # Get the predicted label
+    predicted_label = np.argmax(output_data)
+    class_names = {0: 'NORMAL', 1: 'PNEUMONIA'}
     predicted_class = class_names[predicted_label]
 
     # Return the prediction result as JSON
