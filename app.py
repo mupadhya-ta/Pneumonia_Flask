f84df6e92841a5e00b7cf25bb26f4957629fe88c
mupadhya-ta <mupadhya@uottawa.ca>
2023-03-01 15:07:52 -0500

    added files

--- app.py
+++ app.py
@@ -1,23 +1,15 @@
+import os
+import pickle
+import numpy as np
+from PIL import Image
 from flask import Flask, jsonify, request
 from werkzeug.utils import secure_filename
-import numpy as np
-import tensorflow.lite as tflite
-import os
+from keras.models import load_model
 
 app = Flask(__name__)
 
-# Load the TFLite model
-model_path = "models/pneumonia.tflite"
-interpreter = tflite.Interpreter(model_path=model_path)
-interpreter.allocate_tensors()
-
-# Get the input and output tensors
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
-
-@app.route('/', methods=['GET'])
-def home():
-    return "Hello From Flask"
+# Load the model
+model = load_model('models/pneumonia.h5')
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -30,23 +22,15 @@ def predict():
     file.save(file_path)
 
     # Load the image and preprocess it
-    img = tflite.keras.preprocessing.image.load_img(file_path, target_size=(128, 128))
-    img = tflite.keras.preprocessing.image.img_to_array(img)
-    img = np.expand_dims(img, axis=0)
-    img = np.float32(img)
+    img = Image.open(file_path)
+    img = img.convert('RGB')  # Convert image to RGB format
+    img = img.resize((128, 128))
+    img_array = np.array(img)
+    img_array = img_array.astype('float32') / 255.0
 
-    # Set the input tensor
-    interpreter.set_tensor(input_details[0]['index'], img)
-
-    # Run the model
-    interpreter.invoke()
-
-    # Get the output tensor
-    output_data = interpreter.get_tensor(output_details[0]['index'])
-    output_data = np.squeeze(output_data)
-
-    # Get the predicted label
-    predicted_label = np.argmax(output_data)
+    # Make a prediction
+    predicted_prob = model.predict(np.expand_dims(img_array, axis=0))[0]
+    predicted_label = np.argmax(predicted_prob)
     class_names = {0: 'NORMAL', 1: 'PNEUMONIA'}
     predicted_class = class_names[predicted_label]
 
